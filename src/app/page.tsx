c83127e3b88f99c438cbf5a56432effd85fc33be
Alper Erkol <erkol.alper@yahoo.com>
2024-11-15 14:03:05 +0300

    feature: start to implement authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,43 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/card";
+import { Label } from "@/app/components/ui/label";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+
 export default function Home() {
-  return <main>Crea assignment</main>;
+  return (
+    <main className="w-full h-full">
+      <Card className="w-[450px]">
+        <CardHeader>
+          <CardTitle>Create project</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" placeholder="Name of your project" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="framework">Framework</Label>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Deploy</Button>
+        </CardFooter>
+      </Card>
+    </main>
+  );
 }
---
+++ src/app/utils/routes.ts
@@ -0,0 +1,4 @@
+const AUTH_ROUTE = "/";
+const PROTECTED_ROUTES = ["/products", "/product"];
+const LOGIN_REDIRECT = "/products";
+const UNAUTHENTICATED_REDIRECT = "/?state=unauthenticated";
